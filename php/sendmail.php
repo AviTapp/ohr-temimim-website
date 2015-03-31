6809dad969659c1ae10f60ed99eac660491a2d36
Avi Tapp <Andrew.tapp@gmail.com>
2015-03-31 16:09:51 -0400

    Zipped and Archived

--- php/sendmail.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$name = strip_tags($_REQUEST['name']);
-	$email = strip_tags($_REQUEST['email']);
-	$message = strip_tags($_REQUEST['message']);
-	mail("ohrtemimimschool@gmail.com", "From OhrTemimimSchool.com", $message, "From: $email" );
-	header('Location: http://www.ohrtemimimschool.com');
-	exit();
-	?>
